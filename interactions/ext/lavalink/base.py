3b2dca9bed5222782d49a0dcb65b74e31f265263
Damego <danyabatueff@gmail.com>
2022-08-04 14:12:03 +0500

    chore: prepare for publish on pypi

--- interactions/ext/lavalink/base.py
+++ interactions/ext/lavalink/base.py
@@ -2,15 +2,14 @@ from interactions.ext.base import Base
 from interactions.ext.version import Version, VersionAuthor
 
 __all__ = ["version", "base"]
+__version__ = "0.0.1"
 
-
-version = Version(version="0.0.1", author=VersionAuthor(name="Damego"))
-
+version = Version(version=__version__, author=VersionAuthor(name="Damego", email="damego.dev@gmail.com"))
 base = Base(
     name="interactions-lavalink",
     version=version,
     link="https://github.com/Damego/interactions-lavalink",
-    description="Lavalink for interactions.py",
+    description="Lavalink and voice support for interactions.py",
     packages=["interactions.ext.lavalink"],
     requirements=["discord-py-interactions>=4.3.0", "lavalink"],
 )
