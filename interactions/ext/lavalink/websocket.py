5facffe47d80ba29eac21107302d1d699434d3c7
Damego <danyabatueff@gmail.com>
2022-08-04 13:40:54 +0500

    docs: add docs for websocket

--- interactions/ext/lavalink/websocket.py
+++ interactions/ext/lavalink/websocket.py
@@ -27,6 +27,18 @@ class VoiceWebSocketClient(WebSocketClient):
     async def connect_voice_channel(
         self, guild_id: int, channel_id: int, self_deaf: bool, self_mute: bool
     ):
+        """
+        Sends packet to websocket for connection to voice channel.
+
+        :param guild_id: The guild id to connect.
+        :type guild_id: int
+        :param channel_id: The channel id to connect.
+        :type channel_id: int
+        :param self_deaf: Whether bot is self deafened
+        :type self_deaf: bool
+        :param self_mute: Whether bot is self muted
+        :type self_mute: bool
+        """
         payload = {
             "op": OpCodeType.VOICE_STATE,
             "d": {
@@ -40,6 +52,13 @@ class VoiceWebSocketClient(WebSocketClient):
         await self._send_packet(payload)
 
     async def disconnect_voice_channel(self, guild_id: int):
+        """
+        Sends packet to websocket for disconnecting from voice channel.
+
+        :param guild_id: The guild id
+        :type guild_id: int
+
+        """
         payload = {
             "op": OpCodeType.VOICE_STATE,
             "d": {"channel_id": None, "guild_id": str(guild_id)},
