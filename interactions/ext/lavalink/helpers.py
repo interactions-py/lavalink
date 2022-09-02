b546fc8c0aa5dbcdefe874a77d2c96c7973e03ab
Damego <danyabatueff@gmail.com>
2022-09-02 20:45:07 +0500

    feat: add `Channel.voice_states` and `Guild.voice_states`

--- interactions/ext/lavalink/helpers.py
+++ interactions/ext/lavalink/helpers.py
@@ -1,13 +1,48 @@
-from interactions import Member
+from typing import Optional, List
+
+from interactions import Member, Guild, Channel
+
 from .models import VoiceState
 
 
 @property
-def voice(self):
+def member_voice(self) -> Optional[VoiceState]:
     """
     Returns member's voice state object.
     """
     return self._client.cache[VoiceState].get(self.id)
 
 
-Member.voice = voice
+Member.voice = member_voice
+
+
+@property
+def channel_voice_states(self) -> Optional[List[VoiceState]]:
+    """
+    Returns list of voice states of the voice channel.
+    """
+    return [
+        voice_state
+        for voice_state in self._client.cache[VoiceState].values.values()
+        if voice_state.channel_id == self.id
+    ]
+
+
+
+Channel.voice_states = channel_voice_states
+
+
+@property
+def guild_voice_states(self) -> Optional[List[VoiceState]]:
+    """
+    Returns list of voice states of the guild.
+    """
+    return [
+        voice_state
+        for voice_state in self._client.cache[VoiceState].values.values()
+        if voice_state.guild_id == self.id
+    ]
+
+
+
+Guild.voice_states = guild_voice_states
