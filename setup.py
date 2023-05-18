71ea3f3e1437cd767b0a3175b7d87610ebb762dc
Damego <danyabatueff@gmail.com>
2023-05-18 19:49:32 +0500

    build: use pyproject to get setup info; add manifest

--- setup.py
+++ setup.py
@@ -1,48 +1,37 @@
-import re
 from codecs import open
+from pathlib import Path
 
-from setuptools import setup
+from setuptools import setup, find_packages
+import tomli
 
-# Package information
-AUTHOR = "Damego"
-AUTHOR_EMAIL = "damego.dev@gmail.com"
-DESCRIPTION = "Lavalink and voice support for interactions.py"
-PROJECT_NAME = "interactions-lavalink"
-MAIN_PACKAGE_NAME = "interactions.ext.lavalink"
-URL = "https://github.com/interactions-py/interactions-lavalink"
-
-with open("README.md", "r", encoding="utf-8") as f:
-    README = f.read()
-
-with open("interactions/ext/lavalink/base.py") as fp:
-    VERSION = re.search('__version__ = "([^"]+)"', fp.read())[1]
+with open("pyproject.toml", "rb") as f:
+    pyproject = tomli.load(f)
 
 setup(
-    name=PROJECT_NAME,
-    version=VERSION,
-    author=AUTHOR,
-    author_email=AUTHOR_EMAIL,
-    description=DESCRIPTION,
+    name=pyproject["tool"]["poetry"]["name"],
+    version=pyproject["tool"]["poetry"]["version"],
+    author="Damego",
+    author_email="damego.dev@gmail.com",
+    description=pyproject["tool"]["poetry"]["description"],
     include_package_data=True,
-    install_requires=["discord-py-interactions>=4.3.2", "lavalink~=4.0.1"],
-    license="GPL-3.0 License",
-    long_description=README,
+    install_requires=["interactions.py>=5.0.0,<6.0.0", "lavalink>=4.0.0,<5.0.0"],
+    license=pyproject["tool"]["poetry"]["license"],
+    long_description=(Path(__file__).parent / "README.md").read_text(),
     long_description_content_type="text/markdown",
-    url=URL,
-    packages=["interactions.ext.lavalink"],
-    python_requires=">=3.8",
+    url="https://github.com/interactions-py/lavalink",
+    packages=find_packages(),
+    python_requires=">=3.10",
     classifiers=[
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Topic :: Internet",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Software Development :: Libraries",
         "Topic :: Utilities",
-    ],
+    ]
 )
