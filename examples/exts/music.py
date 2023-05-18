5ef9a53cf5e886d6a00e38133b2c8d6121080421
Damego <danyabatueff@gmail.com>
2023-05-18 19:32:39 +0500

    docs: update examples and readme

--- examples/exts/music.py
+++ examples/exts/music.py
@@ -1,48 +1,45 @@
-from interactions import (
-    CommandContext,
-    Extension,
-    VoiceState,
-    extension_command,
-    extension_listener,
-    option,
-)
-from interactions.ext.lavalink import Lavalink
+from interactions import Extension, SlashContext, listen, slash_command, slash_option
+
+from interactions_lavalink import Lavalink
+from interactions_lavalink.events import TrackStart
 
 
 class Music(Extension):
     def __init__(self, client):
         self.client = client
-        self.lavalink: Lavalink = None
+        self.lavalink: Lavalink | None = None
 
-    @extension_listener()
-    async def on_start(self):
-        # Initialize lavalink instance
+    @listen()
+    async def on_startup(self):
+        # Initializing lavalink instance on bot startup
         self.lavalink: Lavalink = Lavalink(self.client)
 
-        # Connect to lavalink server
+        # Connecting to local lavalink server
         self.lavalink.add_node("127.0.0.1", 43421, "your_password", "eu")
 
-    @extension_command()
-    @option()
-    async def play(self, ctx: CommandContext, query: str):
+    @listen()
+    async def on_track_start(self, event: TrackStart):
+        print("Track started", event.track.title)
+
+    @slash_command()
+    @slash_option("query", "The search query or url", opt_type=3, required=True)
+    async def play(self, ctx: SlashContext, query: str):
         await ctx.defer()
 
         # Getting user's voice state
-        voice_state: VoiceState = ctx.author.voice_state
-        if not voice_state or not voice_state.joined:
+        voice_state = ctx.author.voice
+        if not voice_state or not voice_state.channel:
             return await ctx.send("You're not connected to the voice channel!")
 
         # Connecting to voice channel and getting player instance
-        player = await self.lavalink.connect(voice_state.guild_id, voice_state.channel_id)
-
+        player = await self.lavalink.connect(voice_state.guild.id, voice_state.channel.id)
         # Getting tracks from youtube
         tracks = await player.search_youtube(query)
-        # Selecting first founded track
         track = tracks[0]
         # Adding track to the queue
         player.add(requester=int(ctx.author.id), track=track)
 
-        # Check if already playing
+        # Check if player is already playing
         if player.is_playing:
             return await ctx.send(f"Added to queue: `{track.title}`")
 
@@ -50,13 +47,9 @@ class Music(Extension):
         await player.play()
         await ctx.send(f"Now playing: `{track.title}`")
 
-    @extension_command()
-    async def leave(self, ctx: CommandContext):
-        # Disconnect from voice channel and remove player
-        await self.lavalink.disconnect(ctx.guild_id)
+    @slash_command()
+    async def leave(self, ctx: SlashContext):
+        # Disconnecting from voice channel
+        await self.lavalink.disconnect(ctx.guild.id)
 
         await ctx.send("Disconnected", ephemeral=True)
-
-
-def setup(client):
-    Music(client)
